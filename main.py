457bb2405039900c5486911339f3d49d571731d2
walatch <jakub.pociask@gmail.com>
2024-08-28 20:19:18 +0200

    added words counting func

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     print(text)
+    print(f"It contains {count_words(text)} words")
 
 
 def get_book_text(path):
@@ -9,5 +10,11 @@ def get_book_text(path):
         return f.read()
 
 
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
 if __name__ == '__main__':
     main()
+
