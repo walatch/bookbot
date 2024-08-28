86f8c63952a8b4cf2ec867574fd0ec6b56aebc41
walatch <jakub.pociask@gmail.com>
2024-08-28 20:13:50 +0200

    read file func

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+if __name__ == '__main__':
+    main()
